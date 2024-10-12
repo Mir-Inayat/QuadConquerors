f4b1344be90a3229cacea072df8557df703e86e0
ICTNaWB <salams7@outlook.com>
2024-10-12 04:20:27 +0000

    signinpages

--- src/layouts/authentication/sign-in/index.js
+++ src/layouts/authentication/sign-in/index.js
@@ -37,8 +37,10 @@ import borders from "assets/theme/base/borders";
 // Authentication layout components
 import CoverLayout from "layouts/authentication/components/CoverLayout";
 
+
 // Images
 import bgSignIn from "assets/images/signInImage.png";
+
 
 function SignIn() {
   const [rememberMe, setRememberMe] = useState(true);
@@ -47,92 +49,32 @@ function SignIn() {
 
   return (
     <CoverLayout
-      title="Nice to see you!"
-      color="white"
-      description="Enter your email and password to sign in"
-      premotto="INSPIRED BY THE FUTURE:"
-      motto="THE VISION UI DASHBOARD"
+      title="Welcome!"
+      description="Choose how you would like to sign in"
       image={bgSignIn}
     >
-      <VuiBox component="form" role="form">
+      <VuiBox textAlign="center">
+        <VuiTypography variant="h5" color="white" mb={4}>
+          Sign in as:
+        </VuiTypography>
         <VuiBox mb={2}>
-          <VuiBox mb={1} ml={0.5}>
-            <VuiTypography component="label" variant="button" color="white" fontWeight="medium">
-              Email
-            </VuiTypography>
-          </VuiBox>
-          <GradientBorder
-            minWidth="100%"
-            padding="1px"
-            borderRadius={borders.borderRadius.lg}
-            backgroundImage={radialGradient(
-              palette.gradients.borderLight.main,
-              palette.gradients.borderLight.state,
-              palette.gradients.borderLight.angle
-            )}
-          >
-            <VuiInput type="email" placeholder="Your email..." fontWeight="500" />
-          </GradientBorder>
+          {/* Link to volunteer sign-up */}
+          <Link to="/authentication/sign-in/VolunteerSignIn" style={{ textDecoration: "none" }}>
+            <VuiButton color="info" fullWidth>
+              Volunteer
+            </VuiButton>
+          </Link>
         </VuiBox>
-        <VuiBox mb={2}>
-          <VuiBox mb={1} ml={0.5}>
-            <VuiTypography component="label" variant="button" color="white" fontWeight="medium">
-              Password
-            </VuiTypography>
-          </VuiBox>
-          <GradientBorder
-            minWidth="100%"
-            borderRadius={borders.borderRadius.lg}
-            padding="1px"
-            backgroundImage={radialGradient(
-              palette.gradients.borderLight.main,
-              palette.gradients.borderLight.state,
-              palette.gradients.borderLight.angle
-            )}
-          >
-            <VuiInput
-              type="password"
-              placeholder="Your password..."
-              sx={({ typography: { size } }) => ({
-                fontSize: size.sm,
-              })}
-            />
-          </GradientBorder>
-        </VuiBox>
-        <VuiBox display="flex" alignItems="center">
-          <VuiSwitch color="info" checked={rememberMe} onChange={handleSetRememberMe} />
-          <VuiTypography
-            variant="caption"
-            color="white"
-            fontWeight="medium"
-            onClick={handleSetRememberMe}
-            sx={{ cursor: "pointer", userSelect: "none" }}
-          >
-            &nbsp;&nbsp;&nbsp;&nbsp;Remember me
-          </VuiTypography>
-        </VuiBox>
-        <VuiBox mt={4} mb={1}>
-          <VuiButton color="info" fullWidth>
-            SIGN IN
-          </VuiButton>
-        </VuiBox>
-        <VuiBox mt={3} textAlign="center">
-          <VuiTypography variant="button" color="text" fontWeight="regular">
-            Don&apos;t have an account?{" "}
-            <VuiTypography
-              component={Link}
-              to="/authentication/sign-up"
-              variant="button"
-              color="white"
-              fontWeight="medium"
-            >
-              Sign up
-            </VuiTypography>
-          </VuiTypography>
+        <VuiBox>
+          {/* Link to organization sign-up */}
+          <Link to="/authentication/sign-in/OrganizationSignIn" style={{ textDecoration: "none" }}>
+            <VuiButton color="info" fullWidth>
+              Organization
+            </VuiButton>
+          </Link>
         </VuiBox>
       </VuiBox>
     </CoverLayout>
   );
 }
-
 export default SignIn;
