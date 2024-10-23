283cc7200d8a1e6b6de411ca8b6ced2a8b616f8b
Shariqzain <shariqzain06@gmail.com>
2024-10-23 18:58:40 +0000

    creating opportunity

--- src/layouts/billing/components/Invoices/index.js
+++ src/layouts/billing/components/Invoices/index.js
@@ -1,54 +1,83 @@
 /*!
-
 =========================================================
-* Vision UI Free React - v1.0.0
+* QuadConquerors - v1.0.0
 =========================================================
 
-* Product Page: https://www.creative-tim.com/product/vision-ui-free-react
-* Copyright 2021 Creative Tim (https://www.creative-tim.com/)
-* Licensed under MIT (https://github.com/creativetimofficial/vision-ui-free-react/blob/master LICENSE.md)
+* Copyright 2024 QuadConquerors (https://www.quadconquerors.com)
+* Licensed under MIT (https://github.com/quadconquerors/license/blob/main/LICENSE.md)
 
-* Design and Coded by Simmmple & Creative Tim
+* Coded by QuadConquerors
 
 =========================================================
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
 */
 
-// @mui material components
 import Card from "@mui/material/Card";
-
-// Vision UI Dashboard React components
 import VuiBox from "components/VuiBox";
 import VuiTypography from "components/VuiTypography";
 import VuiButton from "components/VuiButton";
+import VuiInput from "components/VuiInput";
 
-// Billing page components
-import Invoice from "layouts/billing/components/Invoice";
-
-function Invoices() {
+function CreateOpportunity() {
   return (
-    <Card id="delete-account" sx={{ height: "100%" }}>
-      <VuiBox mb="28px" display="flex" justifyContent="space-between" alignItems="center">
+    <Card id="create-opportunity" sx={{ height: "100%" }}>
+      <VuiBox mb="28px">
         <VuiTypography variant="h6" fontWeight="medium" color="white">
-          Invoices
+          Creating an opportunity
         </VuiTypography>
-        <VuiButton variant="contained" color="info" size="small">
-          VIEW ALL
-        </VuiButton>
       </VuiBox>
       <VuiBox>
-        <VuiBox component="ul" display="flex" flexDirection="column" p={0} m={0}>
-          <Invoice date="March, 01, 2020" id="#MS-415646" price="$180" />
-          <Invoice date="February, 10, 2021" id="#RV-126749" price="$250" />
-          <Invoice date="April, 05, 2020" id="#QW-103578" price="$120" />
-          <Invoice date="June, 25, 2019" id="#MS-415646" price="$180" />
-          <Invoice date="March, 01, 2019" id="#AR-803481" price="$300" noGutter />
+        <VuiBox component="form" display="flex" flexDirection="column" gap={2}>
+          <VuiBox>
+            <VuiTypography variant="caption" color="text" fontWeight="medium" mb={1}>
+              Title of event
+            </VuiTypography>
+            <VuiInput placeholder="Enter event title" />
+          </VuiBox>
+          <VuiBox>
+            <VuiTypography variant="caption" color="text" fontWeight="medium" mb={1}>
+              Description of event
+            </VuiTypography>
+            <VuiInput placeholder="Enter event description" multiline rows={3} />
+          </VuiBox>
+          <VuiBox>
+            <VuiTypography variant="caption" color="text" fontWeight="medium" mb={1}>
+              Duration/dates
+            </VuiTypography>
+            <VuiInput placeholder="Enter duration or dates" />
+          </VuiBox>
+          <VuiBox>
+            <VuiTypography variant="caption" color="text" fontWeight="medium" mb={1}>
+              Location of the event
+            </VuiTypography>
+            <VuiInput placeholder="Enter event location" />
+          </VuiBox>
+          <VuiBox>
+            <VuiTypography variant="caption" color="text" fontWeight="medium" mb={1}>
+              Required skills (if any)
+            </VuiTypography>
+            <VuiInput placeholder="Enter required skills" />
+          </VuiBox>
+          <VuiBox>
+            <VuiTypography variant="caption" color="text" fontWeight="medium" mb={1}>
+              Prizes
+            </VuiTypography>
+            <VuiInput placeholder="Enter prizes" />
+          </VuiBox>
+          <VuiBox>
+            <VuiTypography variant="caption" color="text" fontWeight="medium" mb={1}>
+              Additional information
+            </VuiTypography>
+            <VuiInput placeholder="Enter any additional information" />
+          </VuiBox>
+          <VuiButton variant="contained" color="info" type="submit">
+            SUBMIT
+          </VuiButton>
         </VuiBox>
       </VuiBox>
     </Card>
   );
 }
 
-export default Invoices;
+export default CreateOpportunity;
